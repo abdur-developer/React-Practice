38a4994465a732be0a0f0a0b93270fc4c7e3f1f5
abdur-git <abdurr3336@gmail.com>
2026-04-09 14:13:58 +0600

    tasbi done

--- src/Tasbi.jsx
+++ src/Tasbi.jsx
@@ -3,14 +3,14 @@ import { useState } from "react"
 export default function Tasbi(){
     let [count, setCount] = useState(0);
     return (
-        <>
+        <div style={{backgroundColor: "white"}}>
             <h3>Tasbi Counter</h3>
-            <h1 style={{color: "#fff"}}>{count}</h1>
+            <h1 style={{color: "black"}}>{count}</h1>
             <div style={{display: "flex", justifyContent: "center"}}>
                 <button onClick={() => setCount(++count)}>Add</button>
                 <button onClick={() => setCount(--count)}>Minus</button>
                 <button onClick={() => setCount(0)}>Reset</button>
             </div>
-        </>
+        </div>
     )
 }
